2fe2c87b3fdcc2f9641448270dc4407df931f15d
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-06-27 15:23:39 +0200

    Feat: add axios request

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Attraverso l’apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 const { createApp } = Vue ;
 
 createApp( {   
@@ -9,5 +10,10 @@ createApp( {
     },
     mounted() {
         console.log("the component is now mounted");
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => { 
+                const result = response.data.response; 
+                console.log(result);
+            });
     }
 }).mount('#app')  
